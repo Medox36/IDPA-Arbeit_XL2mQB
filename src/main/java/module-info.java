9df0f5b0ffcb13943df1227ce2986d7ab0adb0b2
Medox36 <giuntinil@bzz.ch>
2023-03-02 13:31:39 +0100

    added project files and updated .gitignore (pom.xml, *.iml, java/ch/ksh/xl2mqb/Main.java, module-info.java, resources/ch/ksh/xlm2qb/placeholder, .idea/*.xml)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module IDPA_Arbeit_XL2mQB {
+    requires org.apache.commons.io;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.base;
+
+    opens ch.ksh.xl2mqb to javafx.graphics;
+    exports ch.ksh.xl2mqb;
+}
