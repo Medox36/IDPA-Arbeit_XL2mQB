6ac5aa66d9ee1363d3fc44c3ebd62459e631d8a0
Medox36 <giuntinil@bzz.ch>
2023-03-02 23:22:05 +0100

    added dependencies org.jfxtras:jmetro-11.6.16 and com.github.Dansoftowner:jSystemThemeDetector-3.8

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module IDPA_Arbeit_XL2mQB {
     requires org.apache.commons.io;
     requires org.apache.poi.poi;
     requires org.apache.poi.ooxml;
+    requires org.jfxtras.styles.jmetro;
     requires javafx.graphics;
     requires javafx.controls;
     requires java.base;
