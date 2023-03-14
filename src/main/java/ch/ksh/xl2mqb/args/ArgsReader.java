b5e81f822dca44c31e291474590d1f18be941533
Niki <vogeln@bzz.ch>
2023-03-14 13:43:08 +0100

    ArgsReader methods with throw UnsupportedOperationException

---
+++ src/main/java/ch/ksh/xl2mqb/args/ArgsReader.java
@@ -0,0 +1,15 @@
+package ch.ksh.xl2mqb.args;
+
+import java.io.File;
+
+public class ArgsReader {
+    private String[] args;
+
+    public ArgsReader argsReader(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+    public File getFileFromArgs() {
+        throw new UnsupportedOperationException();
+    }
+}
