f506a9870072a5a15dcace87d87b9f6fbf111a4d
Niki <vogeln@bzz.ch>
2023-03-16 00:06:41 +0100

    improved UML

--- src/main/java/ch/ksh/xl2mqb/analysis/ShortAnswerAnalyser.java
+++ src/main/java/ch/ksh/xl2mqb/analysis/ShortAnswerAnalyser.java
@@ -1,16 +1,8 @@
 package ch.ksh.xl2mqb.analysis;
 
-import ch.ksh.xl2mqb.excel.ExcelHandler;
-
-import org.apache.logging.log4j.core.Logger;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-
 import java.io.File;
 
 public class ShortAnswerAnalyser extends Analyser {
-    protected Logger logger;
-    protected XSSFCell[][] cells;
-    protected ExcelHandler excelHandler;
 
     @Override
     public void analyse(File file) {
