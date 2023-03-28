063fe1c1fff1f78de74b4f3f0513c9211eb403eb
Medox36 <giuntinil@bzz.ch>
2023-03-28 20:15:10 +0200

    added and implemented XMLUtil method for image tag and ImageConverter class

---
+++ src/main/java/ch/ksh/xl2mqb/image/ImageConverter.java
@@ -0,0 +1,34 @@
+package ch.ksh.xl2mqb.image;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+public class ImageConverter {
+
+    public static String convertIfNecessary(String image) {
+        return isRemoteImage(image) ? image : convertToBase64(image);
+    }
+
+    public static boolean isRemoteImage(String image) {
+        return image.startsWith("http");
+    }
+
+    private static String convertToBase64(String image) {
+        try {
+            byte[] imageContent = Files.readAllBytes(Path.of(image));
+            String base64Encoded = Base64.getEncoder().encodeToString(imageContent);
+            String prefix = "data:image/"+ getFileType(image) + ";base64,";
+            return prefix + base64Encoded;
+        } catch (IOException e) {
+            e.printStackTrace();
+            // TODO log exception
+            return "";
+        }
+    }
+
+    private static String getFileType(String image) {
+        return image.substring(image.lastIndexOf(".")).toLowerCase();
+    }
+}
--- src/main/java/ch/ksh/xl2mqb/xml/XMLUtil.java
+++ src/main/java/ch/ksh/xl2mqb/xml/XMLUtil.java
@@ -1,5 +1,7 @@
 package ch.ksh.xl2mqb.xml;
 
+import ch.ksh.xl2mqb.image.ImageConverter;
+
 public class XMLUtil {
     public static final String xmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 
@@ -19,5 +21,21 @@ public class XMLUtil {
 
     public static String getXMLForTextTag(String value, String... attributes) {
         return getXMLForTag("text", value, attributes);
+    }
+
+    public static String getXMLForImgTag(String src, String alt, String... otherAttributes) {
+        StringBuilder xml = new StringBuilder();
+
+        src = ImageConverter.convertIfNecessary(src);
+
+        xml.append("<img");
+        xml.append(" src=\"").append(src).append("\"");
+        xml.append(" alt=\"").append(alt).append("\"");
+        for (String attribute : otherAttributes) {
+            xml.append(" ").append(attribute);
+        }
+        xml.append(">");
+
+        return xml.toString();
     }
 }
