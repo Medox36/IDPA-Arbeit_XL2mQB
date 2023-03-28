eede17831cdbf326e6bcbce3132c65053c79c357
Medox36 <giuntinil@bzz.ch>
2023-03-28 18:01:58 +0200

    added and implemented XMLUtil class

---
+++ src/main/java/ch/ksh/xl2mqb/xml/XMLUtil.java
@@ -0,0 +1,19 @@
+package ch.ksh.xl2mqb.xml;
+
+public class XMLUtil {
+    public static final String xmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
+    public static String getXMLForTag(String tag, String value, String... attributes) {
+        StringBuilder xml = new StringBuilder();
+
+        xml.append("<").append(tag);
+        for (String attribute : attributes) {
+            xml.append(" ").append(attribute);
+        }
+        xml.append(">");
+        xml.append(value);
+        xml.append("</").append(tag).append(">");
+
+        return xml.toString();
+    }
+}
