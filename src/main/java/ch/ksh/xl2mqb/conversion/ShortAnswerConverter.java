e33e390c8bc5a6a3e90b3e590cd1312e19d523c0
Niki <vogeln@bzz.ch>
2023-03-28 21:40:20 +0200

    ShortAnswerConverter is implemented with image but not tested, fixes #11

--- src/main/java/ch/ksh/xl2mqb/conversion/ShortAnswerConverter.java
+++ src/main/java/ch/ksh/xl2mqb/conversion/ShortAnswerConverter.java
@@ -22,45 +22,47 @@ public class ShortAnswerConverter extends Converter {
     }
 
     private String convertSingleQuestion(XSSFRow row){
-
+        int rownum = 1;
         String aShortAnswer = "<question type=\"shortanswer\">";
 
         for (int colI = 0; colI < row.getLastCellNum(); colI++) {
             XSSFCell cell = row.getCell(colI);
             switch (colI) {
                 case 0 -> {
-                    aShortAnswer += "<name><text>" + row.getCell(colI).getStringCellValue() + "</text>\n</name>";
+                    aShortAnswer += "<name><text>" + row.getCell(colI).getStringCellValue().trim() + "</text>\n</name>";
                 }
                 case 1 -> {
-                    aShortAnswer += "<defaultgrade>" + row.getCell(colI).getStringCellValue() + "</defaultgrade>";
+                    aShortAnswer += "<defaultgrade>" + row.getCell(colI).getStringCellValue().trim() + "</defaultgrade>";
+                    aShortAnswer += "<penalty>0.0000000</penalty>";
+                    aShortAnswer += "<idnumber>" + rownum + "</idnumber>";
                 }
                 case 2 -> {
                         aShortAnswer += "<generalfeedback format=\"moodle_auto_format\">\n<text>"
-                                + row.getCell(colI).getStringCellValue()
+                                + row.getCell(colI).getStringCellValue().trim()
                                 + "</text></generalfeedback>";
                 }
                 case 4 -> {
                     if (row.getCell(3).getStringCellValue() == null || row.getCell(4).getStringCellValue() == "") {
                         aShortAnswer += "<questiontext format=\"moodle_auto_format\"><text>"
-                                + row.getCell(colI).getStringCellValue()
+                                + row.getCell(colI).getStringCellValue().trim()
                                 + "</text></questiontext>";
                     } else {
                         aShortAnswer += "<questiontext format=\"moodle_auto_format\"><text>"
-                                + row.getCell(colI).getStringCellValue()
-                                + "<img src=\"" + row.getCell(3).getStringCellValue()
-                                + "\" alt=\"image\" role=\"presentation\" class=\"atto_image_button_text-bottom\">"
+                                + row.getCell(colI).getStringCellValue().trim()
+                                + XMLUtil.getXMLForImgTag(row.getCell(3).getStringCellValue().trim(), "image", "role=\"presentation\"", "class=\"atto_image_button_text-bottom\"")
                                 + "</text></questiontext>";
                     }
                 }
                 case 5, 8, 11, 14, 17, 20, 23, 26, 29, 32 -> {
-                    aShortAnswer += XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue())
-                            + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI+2).getStringCellValue(),
-                            "format=\"moodle_auto_format\"")) , "fraction=\"" + row.getCell(colI+1).getStringCellValue()
+                    aShortAnswer += XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())
+                            + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI+2).getStringCellValue().trim(),
+                            "format=\"moodle_auto_format\"")) , "fraction=\"" + row.getCell(colI+1).getStringCellValue().trim()
                             + "\"", "format=\"moodle_auto_format\"");
                 }
             }
         }
         aShortAnswer += "</question>";
+        rownum++;
         return aShortAnswer;
     }
 }
