4db2797a20186b9789695fbf267d7dea9ca03685
Niki <vogeln@bzz.ch>
2023-03-30 19:34:50 +0200

    ShortAnswerConverter is now using Type and Format class

--- src/main/java/ch/ksh/xl2mqb/conversion/ShortAnswerConverter.java
+++ src/main/java/ch/ksh/xl2mqb/conversion/ShortAnswerConverter.java
@@ -1,10 +1,9 @@
 package ch.ksh.xl2mqb.conversion;
 
-import ch.ksh.xl2mqb.excel.ExcelHandler;
-import ch.ksh.xl2mqb.xml.XMLUtil;
+
+import ch.ksh.xl2mqb.conversion.xml.XMLUtil;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ShortAnswerConverter extends Converter {
 
@@ -22,10 +21,9 @@ public class ShortAnswerConverter extends Converter {
     }
 
     private String convertSingleQuestion(XSSFRow row){
-        String aShortAnswer = "<question type=\"shortanswer\">";
+        String aShortAnswer = "<question "+Type.SHORTANSWER+">";
 
         for (int colI = 0; colI < row.getLastCellNum(); colI++) {
-            XSSFCell cell = row.getCell(colI);
             switch (colI) {
                 case 0 -> {
                     if (checkRow(row)) {
@@ -36,22 +34,24 @@ public class ShortAnswerConverter extends Converter {
                         aShortAnswer += XMLUtil.getXMLForTag("defaultgrade", row.getCell(colI).getStringCellValue().trim());
                 }
                 case 2 -> {
-                        aShortAnswer += XMLUtil.getXMLForTag("generalfeedback", XMLUtil.getXMLForTag("text", row.getCell(colI).getStringCellValue().trim()),
-                                "format=\"moodle_auto_format\"");
+                        aShortAnswer += XMLUtil.getXMLForTag("generalfeedback", XMLUtil.getXMLForTag("text",
+                                row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT);
                 }
                 case 4 -> {
                     if (checkRow(row)) {
                         if (row.getCell(3).getStringCellValue() == null || row.getCell(4).getStringCellValue().equals("")) {
-                            aShortAnswer += XMLUtil.getXMLForTag("questiontext", XMLUtil.getXMLForTag("text", row.getCell(colI).getStringCellValue().trim()), "format=\"moodle_auto_format\"");
+                            aShortAnswer += XMLUtil.getXMLForTag("questiontext",
+                                    XMLUtil.getXMLForTag("text", row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT);
                         } else {
                             aShortAnswer += XMLUtil.getXMLForTag("questiontext", XMLUtil.getXMLForTag("text", row.getCell(colI).getStringCellValue().trim()
-                                            + XMLUtil.getXMLForImgTag(row.getCell(3).getStringCellValue().trim(), "image", "role=\"presentation\"", "class=\"atto_image_button_text-bottom\"")),
-                                    "format=\"moodle_auto_format\"");
+                                            + XMLUtil.getXMLForImgTag(row.getCell(3).getStringCellValue().trim(),
+                                    "image", "role=\"presentation\"", "class=\"atto_image_button_text-bottom\"")), Format.AUTO_FORMAT);
                         }
                     }
                 }
                 case 5 -> {
-                    aShortAnswer += XMLUtil.getXMLForTag("hint", XMLUtil.getXMLForTag("text",row.getCell(colI).getStringCellValue().trim()), "format=\"moodle_auto_format\"");
+                    aShortAnswer += XMLUtil.getXMLForTag("hint", XMLUtil.getXMLForTag("text",
+                            row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT);
                 }
                 case 6 -> {
                     aShortAnswer += XMLUtil.getXMLForTag("penalty", row.getCell(colI).getStringCellValue().trim());
@@ -60,15 +60,15 @@ public class ShortAnswerConverter extends Converter {
                     if (checkRow(row)) {
                         aShortAnswer += XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())
                                 + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI + 2).getStringCellValue().trim(),
-                                "format=\"moodle_auto_format\"")), "fraction=\"" + row.getCell(colI + 1).getStringCellValue().trim()
-                                + "\"", "format=\"moodle_auto_format\"");
+                                Format.AUTO_FORMAT)), "fraction=\"" + row.getCell(colI + 1).getStringCellValue().trim()
+                                + "\"", Format.AUTO_FORMAT);
                     }
                 }
                 case 10, 13, 16, 19, 22, 25, 28, 31, 34-> {
                     aShortAnswer += XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())
                             + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI+2).getStringCellValue().trim(),
-                            "format=\"moodle_auto_format\"")) , "fraction=\"" + row.getCell(colI+1).getStringCellValue().trim()
-                            + "\"", "format=\"moodle_auto_format\"");
+                            Format.AUTO_FORMAT)) , "fraction=\"" + row.getCell(colI+1).getStringCellValue().trim()
+                            + "\"", Format.AUTO_FORMAT);
                 }
             }
         }
