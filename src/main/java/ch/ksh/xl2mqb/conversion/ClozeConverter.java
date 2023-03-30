94616f3c2e1c1e26f16fcfce7d1bcb3df11a4c56
Medox36 <giuntinil@bzz.ch>
2023-03-30 19:23:43 +0200

    updated ClozeConverter to match new excel template and added logging checking for mandatory/necessary cells to not be empty

--- src/main/java/ch/ksh/xl2mqb/conversion/ClozeConverter.java
+++ src/main/java/ch/ksh/xl2mqb/conversion/ClozeConverter.java
@@ -7,8 +7,10 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class ClozeConverter extends Converter {
+    private final Pattern numericPattern = Pattern.compile("\\d+");
 
     private final XSSFSheet subQuestionSheet;
 
@@ -20,18 +22,63 @@ public class ClozeConverter extends Converter {
 
     public String convert() {
         for (int rowI = 1; rowI < sheet.getLastRowNum(); rowI++) {
-            convertSingleQuestion(sheet.getRow(rowI));
+            XSSFRow row = sheet.getRow(rowI);
+            if (hasAllNecessaryContents(row)) {
+                convertSingleQuestion(row);
+            } else {
+                logger.info("Die Frage auf Zeile " + (row.getRowNum() + 1)
+                        + " vom Typ Cloze hat nicht alle benötigten Daten.");
+            }
         }
 
         return xmlString;
     }
 
+    private boolean hasAllNecessaryContents(XSSFRow row) {
+        // does the question have a Name
+        if (row.getCell(0).getStringCellValue().isBlank()) {
+            return false;
+        }
+        // does the question have a formulation of a question
+        if (row.getCell(5).getStringCellValue().isBlank()) {
+            return false;
+        }
+        // does the question have at least one sub-question
+        if (!hasSubQuestions(row)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean hasSubQuestions(XSSFRow row) {
+        boolean hasSubQuestions = false;
+        for (int i = 6; i < row.getRowNum(); i++) {
+            String cellValue = row.getCell(i).getStringCellValue().trim();
+            if (!cellValue.isBlank() && !isNumeric(cellValue)) {
+                hasSubQuestions = true;
+                break;
+            }
+        }
+
+        return hasSubQuestions;
+    }
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+
+        return numericPattern.matcher(strNum).matches();
+    }
+
     private void convertSingleQuestion(XSSFRow row) {
-        xmlString += "<question type=\"cloze\">";
+        xmlString += "<question " + Type.CLOZE + ">";
 
         questionName(row.getCell(0));
-        penalty(row.getCell(1));
-        generalFeedback(row.getCell(2));
+        generalFeedback(row.getCell(1));
+        hint(row.getCell(3));
+        penalty(row.getCell(4));
         questionText(row);
 
         xmlString += "</question>";
@@ -42,20 +89,25 @@ public class ClozeConverter extends Converter {
         xmlString += XMLUtil.getXMLForTag("name", text);
     }
 
+    private void hint(XSSFCell cell) {
+        String text = XMLUtil.getXMLForTag("text", cell.getStringCellValue().trim());
+        xmlString += XMLUtil.getXMLForTag("generalfeedback ", text, Format.AUTO_FORMAT);
+    }
+
     private void penalty(XSSFCell cell) {
         xmlString += XMLUtil.getXMLForTag("penalty", cell.getStringCellValue().trim());
     }
 
     private void generalFeedback(XSSFCell cell) {
         String text = XMLUtil.getXMLForTextTag(cell.getStringCellValue().trim());
-        xmlString += XMLUtil.getXMLForTag("generalfeedback ", text, "format=\"moodle_auto_format\"");
+        xmlString += XMLUtil.getXMLForTag("generalfeedback ", text, Format.AUTO_FORMAT);
     }
 
     private void questionText(XSSFRow row) {
-        xmlString += "<questiontext format=\"moodle_auto_format\">";
+        xmlString += "<questiontext " + Format.AUTO_FORMAT + ">";
 
-        nonSubQuestionText(row.getCell(4));
-        picture(row.getCell(3));
+        nonSubQuestionText(row.getCell(5));
+        picture(row.getCell(2));
 
         boolean skipped = true;
         for (int cellI = 5; cellI < row.getLastCellNum(); cellI++) {
@@ -64,7 +116,8 @@ public class ClozeConverter extends Converter {
         }
 
         if (skipped) {
-            // TODO show in log that no sub question was selected
+            logger.info("Für die Frage auf Zeile " + (row.getRowNum() + 1)
+                    + " vom Typ Cloze wurde(n) keine Teilfragen angegeben.");
         }
 
         xmlString += "</questiontext>";
@@ -82,13 +135,14 @@ public class ClozeConverter extends Converter {
     }
 
     private void subQuestion(XSSFCell cell) {
-        xmlString += convertSubQuestion(cell.getStringCellValue().trim());
+        xmlString += convertSubQuestion(cell.getStringCellValue().trim(), cell.getRowIndex());
     }
 
-    private String convertSubQuestion(String questionNumber) {
+    private String convertSubQuestion(String questionNumber, int rowNum) {
         XSSFRow row = getRowForMatchingQuestionNumber(questionNumber);
         if (row == null) {
-            // TODO show error sub question doesn't exist
+            logger.info("Für die Frage auf Zeile " + (rowNum + 1)
+                    + " vom Typ Cloze wurde keine passende Teilfrage mit der Nummer " + questionNumber + " gefunden.");
             return "";
         }
 
