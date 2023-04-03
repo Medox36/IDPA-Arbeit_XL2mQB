8c0f4805b6dc228d59f398482598c4ae71c6bec7
Niki <vogeln@bzz.ch>
2023-04-03 19:25:15 +0200

    ShortAnswerConverter has usecase implemented

--- src/main/java/ch/ksh/xl2mqb/conversion/ShortAnswerConverter.java
+++ src/main/java/ch/ksh/xl2mqb/conversion/ShortAnswerConverter.java
@@ -1,8 +1,7 @@
 package ch.ksh.xl2mqb.conversion;
 
-
 import ch.ksh.xl2mqb.conversion.xml.XMLUtil;
-import org.apache.poi.xssf.usermodel.XSSFCell;
+
 import org.apache.poi.xssf.usermodel.XSSFRow;
 
 public class ShortAnswerConverter extends Converter {
@@ -17,74 +16,91 @@ public class ShortAnswerConverter extends Converter {
             XSSFRow row = sheet.getRow(rowI);
             convertSingleQuestion(row);
         }
+
         return xmlString;
     }
 
-    private String convertSingleQuestion(XSSFRow row){
-        String aShortAnswer = "<question "+Type.SHORTANSWER+">";
+    private void convertSingleQuestion(XSSFRow row) {
+        StringBuilder aShortAnswerBuilder = new StringBuilder("<question " + Type.SHORTANSWER + ">");
+
+        if (!checkRow(row)) {
+            logger.info("Die Frage auf Zeile " + (row.getRowNum() + 1)
+                    + " vom Typ Shortanswer hat nicht alle benötigten Daten.");
+            return;
+        }
 
         for (int colI = 0; colI < row.getLastCellNum(); colI++) {
             switch (colI) {
+                //question name
                 case 0 -> {
-                    if (checkRow(row)) {
-                        aShortAnswer += XMLUtil.getXMLForTag("name", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim()));
-                    }
+                    aShortAnswerBuilder.append(XMLUtil.getXMLForTag("name", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())));
                 }
+                //points
                 case 1 -> {
-                        aShortAnswer += XMLUtil.getXMLForTag("defaultgrade", row.getCell(colI).getStringCellValue().trim());
+                    aShortAnswerBuilder.append(XMLUtil.getXMLForTag("defaultgrade", row.getCell(colI).getStringCellValue().trim()));
                 }
+                //feedback
                 case 2 -> {
-                        aShortAnswer += XMLUtil.getXMLForTag("generalfeedback", XMLUtil.getXMLForTag("text",
-                                row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT);
+                    aShortAnswerBuilder.append(XMLUtil.getXMLForTag("generalfeedback", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT));
                 }
+                //case sensitive
                 case 4 -> {
-                    if (checkRow(row)) {
-                        if (row.getCell(3).getStringCellValue() == null || row.getCell(4).getStringCellValue().equals("")) {
-                            aShortAnswer += XMLUtil.getXMLForTag("questiontext",
-                                    XMLUtil.getXMLForTag("text", row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT);
-                        } else {
-                            aShortAnswer += XMLUtil.getXMLForTag("questiontext", XMLUtil.getXMLForTag("text", row.getCell(colI).getStringCellValue().trim()
-                                            + XMLUtil.getXMLForImgTag(row.getCell(3).getStringCellValue().trim(),
-                                    "image", "role=\"presentation\"", "class=\"atto_image_button_text-bottom\"")), Format.AUTO_FORMAT);
-                        }
+                    if (row.getCell(colI).getStringCellValue().trim().equals("Ja")) {
+                        aShortAnswerBuilder.append(XMLUtil.getXMLForTag("usecase","1"));
+                    } else {
+                        aShortAnswerBuilder.append(XMLUtil.getXMLForTag("usecase","0"));
                     }
                 }
+                //hint
                 case 5 -> {
-                    aShortAnswer += XMLUtil.getXMLForTag("hint", XMLUtil.getXMLForTag("text",
-                            row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT);
+                    aShortAnswerBuilder.append(XMLUtil.getXMLForTag("hint", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT));
                 }
+                //penalty
                 case 6 -> {
-                    aShortAnswer += XMLUtil.getXMLForTag("penalty", row.getCell(colI).getStringCellValue().trim());
+                    aShortAnswerBuilder.append(XMLUtil.getXMLForTag("penalty", row.getCell(colI).getStringCellValue().trim()));
                 }
+                //question with picture
                 case 7 -> {
-                    if (checkRow(row)) {
-                        aShortAnswer += XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())
-                                + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI + 2).getStringCellValue().trim(),
-                                Format.AUTO_FORMAT)), "fraction=\"" + row.getCell(colI + 1).getStringCellValue().trim()
-                                + "\"", Format.AUTO_FORMAT);
+                    if (row.getCell(3).getStringCellValue() == null || row.getCell(4).getStringCellValue().equals("")) {
+                        aShortAnswerBuilder.append(XMLUtil.getXMLForTag("questiontext",
+                                XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim()), Format.AUTO_FORMAT));
+                    } else {
+                        aShortAnswerBuilder.append(XMLUtil.getXMLForTag("questiontext", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim() + XMLUtil.getXMLForImgTag(row.getCell(3).getStringCellValue().trim(),
+                                "image", "role=\"presentation\"", "class=\"atto_image_button_text-bottom\"")), Format.AUTO_FORMAT));
                     }
                 }
-                case 10, 13, 16, 19, 22, 25, 28, 31, 34-> {
-                    aShortAnswer += XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())
-                            + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI+2).getStringCellValue().trim(),
-                            Format.AUTO_FORMAT)) , "fraction=\"" + row.getCell(colI+1).getStringCellValue().trim()
-                            + "\"", Format.AUTO_FORMAT);
+                //question answers
+                case 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 -> {
+                    aShortAnswerBuilder.append(XMLUtil.getXMLForTag("answer", XMLUtil.getXMLForTextTag(row.getCell(colI).getStringCellValue().trim())
+                            + XMLUtil.getXMLForTag("feedback", XMLUtil.getXMLForTextTag(row.getCell(colI + 2).getStringCellValue().trim(),
+                            Format.AUTO_FORMAT)), "fraction=\"" + row.getCell(colI + 1).getStringCellValue().trim()
+                            + "\"", Format.AUTO_FORMAT));
                 }
             }
         }
-        aShortAnswer += "</question>";
+        aShortAnswerBuilder.append("</question>");
 
-        return aShortAnswer;
+        xmlString += aShortAnswerBuilder.toString();
     }
 
-    private boolean checkRow(XSSFRow row){
-        for (int colI = 0; colI < row.getLastCellNum(); colI++){
-            if (row.getCell(colI).getStringCellValue().trim().isBlank()){
-                logger.info("Die Frage auf Zeile "+ (row.getRowNum() + 1)
-                        + " vom Typ Shortanswer hat nicht alle benötigten Daten.");
-                return false;
-            }
+    private boolean checkRow(XSSFRow row) {
+        // does the question have a Name
+        if (row.getCell(0).getStringCellValue().isBlank()) {
+            return false;
         }
+        // does the question have a formulation of a question
+        if (row.getCell(7).getStringCellValue().isBlank()) {
+            return false;
+        }
+        // does the question have answer
+        if (row.getCell(8).getStringCellValue().isBlank()) {
+            return false;
+        }
+        // does the question have a fraction for the points of one answer
+        if (row.getCell(9).getStringCellValue().isBlank()) {
+            return false;
+        }
+
         return true;
     }
 }
