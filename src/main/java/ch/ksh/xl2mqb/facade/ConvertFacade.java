664312d5733c22d32642cc9a1b121afc403249ed
Niki <vogeln@bzz.ch>
2023-04-04 22:27:45 +0200

    starting of conversion in seperate thread, #29

--- src/main/java/ch/ksh/xl2mqb/facade/ConvertFacade.java
+++ src/main/java/ch/ksh/xl2mqb/facade/ConvertFacade.java
@@ -1,5 +1,9 @@
 package ch.ksh.xl2mqb.facade;
 
+import ch.ksh.xl2mqb.conversion.ClozeConverter;
+import ch.ksh.xl2mqb.conversion.MultipleChoiceConverter;
+import ch.ksh.xl2mqb.conversion.ShortAnswerConverter;
+import ch.ksh.xl2mqb.conversion.xml.XMLUtil;
 import ch.ksh.xl2mqb.gui.AlertUtil;
 import ch.ksh.xl2mqb.gui.ProgressContainer;
 import ch.ksh.xl2mqb.gui.XL2mQB;
@@ -8,12 +12,16 @@ import ch.ksh.xl2mqb.log.TextAreaAppender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javafx.application.Platform;
 import javafx.scene.control.ButtonType;
 
 import java.util.Optional;
 
 public class ConvertFacade {
     private static ConvertFacade INSTANCE;
+    private Thread converterThread;
+    private String xml;
+    private volatile boolean isConversionFinished;
     private XL2mQB gui;
     private final Logger logger = LogManager.getLogger(TextAreaAppender.class);
 
@@ -27,7 +35,27 @@ public class ConvertFacade {
         progressContainer.clearTextArea();
         progressContainer.setProgress(0.0);
 
+        isConversionFinished = false;
+        xml = "";
 
+        converterThread = new Thread(() -> {
+            xml = XMLUtil.xmlHeader;
+            String convertedQuestions = "";
+
+            convertedQuestions += new MultipleChoiceConverter().convert();
+            convertedQuestions += new ShortAnswerConverter().convert();
+            convertedQuestions += new ClozeConverter().convert();
+
+            xml += XMLUtil.getXMLForTag("quiz", convertedQuestions);
+
+            isConversionFinished = true;
+
+            Platform.runLater(() -> {
+                gui.getProgressContainer().setProgress(1.0);
+                gui.convertFinishScene();
+            });
+        }, "Converter-Thread");
+        converterThread.start();
     }
 
     public void cancelConversion() {
@@ -35,18 +63,23 @@ public class ConvertFacade {
 
         if (buttonType.isPresent()) {
             if (buttonType.get() == ButtonType.YES) {
-                // TODO cancel conversion
+                converterThread.interrupt();
+                converterThread = null;
                 gui.homeScene();
             }
         }
     }
 
-    private void finishConversion() {
-        throw new UnsupportedOperationException();
-    }
-
     public Logger getLogger() {
         return logger;
+    }
+
+    public boolean isConversionFinished() {
+        return isConversionFinished;
+    }
+
+    public String getXml() {
+        return xml;
     }
 
     public static ConvertFacade getInstance() {
