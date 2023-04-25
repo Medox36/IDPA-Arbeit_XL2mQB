9a85d586db6ae2a321ab37da6ac8010703a71bf7
Niki <vogeln@bzz.ch>
2023-04-25 15:38:00 +0200

    ShortAnswerAnalyser fixed base 1 to 0

--- src/main/java/ch/ksh/xl2mqb/analysis/ShortAnswerAnalyser.java
+++ src/main/java/ch/ksh/xl2mqb/analysis/ShortAnswerAnalyser.java
@@ -17,16 +17,16 @@ public class ShortAnswerAnalyser extends Analyser {
 
     @Override
     public void analyse() {
-        for (int rowI = 1; rowI < sheet.getLastRowNum(); rowI++) {
+        for (int rowI = 0; rowI < sheet.getLastRowNum(); rowI++) {
             XSSFRow row = sheet.getRow(rowI);
-            AnalyserUtil.questionName(shortanswerAnalyseResult, row.getCell(1), row.getRowNum());
-            AnalyserUtil.points(shortanswerAnalyseResult, row.getCell(2), row.getRowNum());
-            AnalyserUtil.generalFeedback(shortanswerAnalyseResult, row.getCell(3), row.getRowNum());
-            AnalyserUtil.picture(shortanswerAnalyseResult, row.getCell(4), row.getRowNum());
-            AnalyserUtil.hintAndPenalty(shortanswerAnalyseResult, row.getCell(6),row.getCell(7), row.getRowNum());
-            AnalyserUtil.questionText(shortanswerAnalyseResult, row.getCell(8), row.getRowNum());
+            AnalyserUtil.questionName(shortanswerAnalyseResult, row.getCell(0), row.getRowNum());
+            AnalyserUtil.points(shortanswerAnalyseResult, row.getCell(1), row.getRowNum());
+            AnalyserUtil.generalFeedback(shortanswerAnalyseResult, row.getCell(2), row.getRowNum());
+            AnalyserUtil.picture(shortanswerAnalyseResult, row.getCell(3), row.getRowNum());
+            AnalyserUtil.hintAndPenalty(shortanswerAnalyseResult, row.getCell(5),row.getCell(7), row.getRowNum());
+            AnalyserUtil.questionText(shortanswerAnalyseResult, row.getCell(7), row.getRowNum());
             for (int i = 1; i < 10; i++) {
-                AnalyserUtil.questionAnswer(shortanswerAnalyseResult, row.getCell(9+step), row.getCell(10+step), row.getCell(11+step), row.getRowNum(), i);
+                AnalyserUtil.questionAnswer(shortanswerAnalyseResult, row.getCell(8+step), row.getCell(9+step), row.getCell(10+step), row.getRowNum(), i);
                 step+=3;
             }
 
