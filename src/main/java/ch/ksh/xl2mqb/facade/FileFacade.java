5abe36e3e13d853748dee47f0018348295119ef3
Medox36 <giuntinil@bzz.ch>
2023-05-01 18:50:49 +0200

    re-reworked saving of xml file, creation of non-existing directories via File#mkdirs lead to exceptions(now fixed) because no check was being made if the directories actually don't exist

--- src/main/java/ch/ksh/xl2mqb/facade/FileFacade.java
+++ src/main/java/ch/ksh/xl2mqb/facade/FileFacade.java
@@ -76,12 +76,13 @@ public class FileFacade {
         Path xmlPath = file.toPath();
         if (Files.notExists(xmlPath)) {
             try {
-                if (file.getParentFile().mkdirs()) {
-                    Files.createFile(xmlPath);
-                } else {
-                    AlertUtil.errorAlert("Fehler beim Speichern", "Es gab einen Fehler beim speichern der Datei.",
-                            "Die ausgewählten Ordner konnten nicht erstellt werden.\nBitter versuchen Sie es erneut oder speichern Sie in einem anderen Ordner.");
+                if (Files.notExists(file.getParentFile().toPath())) {
+                    if (!file.getParentFile().mkdirs()) {
+                        AlertUtil.errorAlert("Fehler beim Speichern", "Es gab einen Fehler beim speichern der Datei.",
+                                "Die ausgewählten Ordner konnten nicht erstellt werden.\nBitter versuchen Sie es erneut oder speichern Sie in einem anderen Ordner.");
+                    }
                 }
+                Files.createFile(xmlPath);
             } catch (IOException e) {
                 e.printStackTrace();
                 AlertUtil.errorAlert("Fehler beim Speichern", "Es gab einen Fehler beim speichern der Datei.",
