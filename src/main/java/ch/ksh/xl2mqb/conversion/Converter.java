4b0e295f42866ff68bbd972b04f5c1d4bda9678a
Medox36 <giuntinil@bzz.ch>
2023-05-02 16:17:53 +0200

    added and implemented method Converter#isRowEmpty

--- src/main/java/ch/ksh/xl2mqb/conversion/Converter.java
+++ src/main/java/ch/ksh/xl2mqb/conversion/Converter.java
@@ -1,10 +1,12 @@
 package ch.ksh.xl2mqb.conversion;
 
+import ch.ksh.xl2mqb.excel.CellExtractor;
 import ch.ksh.xl2mqb.excel.ExcelHandler;
 import ch.ksh.xl2mqb.facade.ConvertFacade;
 import ch.ksh.xl2mqb.facade.FileFacade;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 public abstract class Converter {
@@ -18,5 +20,13 @@ public abstract class Converter {
     }
 
     public abstract String convert();
-}
 
+    protected boolean isRowEmpty(XSSFRow row) {
+        for (int i = 0; i < row.getLastCellNum(); i++) {
+            if (!CellExtractor.getCellValueSafe(row.getCell(i)).isBlank()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
